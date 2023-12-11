a7f32beb5362262e82f625f594967e1b1bd82db2
Mansi Bhiwapurkar <mansibhiwapurkar97@gmail.com>
2023-12-10 20:38:02 -0800

    inserting the contact

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from app import views
 urlpatterns = [
     path('',views.view_list_page,name='view_list_page'),
     path('create',views.create,name='create'),
+     path('insert',views.insertData,name='insertData'),
 ]
--- app/views.py
+++ app/views.py
@@ -12,3 +12,26 @@ def view_list_page(request):
 
 def create(request):
     return render(request,"create.html")
+
+def insertData(request):
+    if request.method=="POST":
+        button_clicked=request.POST.get('button')
+        if button_clicked=='cancel':
+            return render('/')
+        else:
+            name=request.POST.get("name")
+            email=request.POST.get("email")
+            notes=request.POST.get("notes")
+            time = timezone.now()
+            data=Contact.objects.all()
+            if Contact.objects.filter(name=name).exists():
+                messages.error(request, 'Username already exists')
+                return render(request,"create.html") 
+            else:
+                print(name,email,notes,time)
+                query=Contact(name=name,email=email,notes=notes,time=time)
+                query.save()  
+                data=Contact.objects.all()
+                context={"data":data}
+                messages.info(request,"Data inserted successfully")
+                return render(request,"list_page.html",context) 
